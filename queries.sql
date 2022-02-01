ce6afb7a1e374de9e839724f671383ee6c194bb0
Brian Aston <astonb.dev@gmail.com>
2022-02-01 12:12:55 -0800

    No more janes

--- queries.sql
+++ queries.sql
@@ -1,1 +1,12 @@
 -- write your queries here
+SELECT *
+FROM owners
+FULL OUTER JOIN vehicles
+ON owners.id = vehicles.owner_id;
+
+SELECT first_name, COUNT(*) AS num_vehicles
+FROM owners
+JOIN vehicles
+ON owners.id = vehicles.owner_id
+GROUP BY owner_id
+ORDER BY first_name;
