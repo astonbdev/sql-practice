25f26c97552500db3cdf36144542ed802f8ef646
edwinkim97 <edwinkim124@gmail.com>
2022-02-01 12:40:15 -0800

    finished part 1

--- queries.sql
+++ queries.sql
@@ -8,5 +8,13 @@ SELECT first_name, COUNT(*) AS num_vehicles
 FROM owners
 JOIN vehicles
 ON owners.id = vehicles.owner_id
-GROUP BY owner_id
+GROUP BY owners.id
 ORDER BY first_name;
+
+SELECT first_name, ROUND(SUM(price)/COUNT(*), 0) AS avg_price_greater_than, COUNT(*) AS count
+FROM vehicles
+JOIN owners
+ON vehicles.owner_id = owners.id
+GROUP BY owners.id
+HAVING COUNT(*) > 1 AND SUM(price)/COUNT(*) > 10000 
+ORDER BY first_name DESC;
